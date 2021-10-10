2c77b3096ae22600a140e1da4bb76c43df955f38
jordanthopps <91483280+jordanthopps@users.noreply.github.com>
2021-10-09 22:29:30 -0400

    Intro

--- js/site.js
+++ js/site.js
@@ -1,16 +1,50 @@
-function displayMessage() {
-    let msg = ""
+//Mortgage Calculator
+//Step 1 - Get User inputs - this will access the DOM and call all other functions
+function getValues() {
 
-    msg = document.getElementById("message").value;
+    let loanAmount = 0
+    let term = 0
+    let interestRate = 0
 
-    alert(msg);
+    loanAmount = document.getElementById("loanAmount").value;
+
+    term = document.getElementById("termLength").value;
+
+    interestRate = document.getElementById("interestRate").value;
+
+    let calculatedRate = calcRate(interestRate);
+
+    let payment = calcPayment(loanAmount, calculatedRate, term);
+
+    let totalPayments = calcPaymentSchedule(loanAmount, calculatedRate, term, payment);
+
+    displayData(totalPayments);
+
 }
 
-function displayMessage2() {
-    let msg = "";
+//Step 2 - Calculate the interest rate - this is the division by 1200 equation from the PDF
+function calcRate() {}
 
-    msg = document.getElementById("message").value;
+//Step 3 - Calculate monthly payment - Amount times rate divided by the complicated math from the PDF Math.pow()
+function calcPayment() {}
 
-    Swal.fire(msg);
+//Step 3.5 - Calculate the interest rate based on the current balance in rate from step 2
+//This function is called in Step 4, but you need to build it first
+//Calculates interest per payment
+function calcInterest() {}
 
-}
+//Step 4 - Now that we know the rate and monthly payment we build out our payment schedule
+//This is the big function of the application - create your object in this function
+//let obj = {
+//     month:
+//     payment:
+//     principal:
+//     interest:
+//     totalInterest:
+//     balance:
+// }
+function calcPaymentSchedule() {}
+
+//Step 5 - Display all of the calculated information
+//Use the template structure from FizzBuzz to output your data
+function displayData() {}
